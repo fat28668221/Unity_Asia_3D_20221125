0aacf6f4c20439be99ce9139c7c14a96707283ef
fat28668221 <96966547+fat28668221@users.noreply.github.com>
2022-12-01 15:04:41 +0800

    202212010304

---
+++ 202211253D_Kid/Assets/Scripts/DialogueData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Justin
+{
+    /// <summary>
+    /// 對話資料
+    /// </summary>
+    [CreateAssetMenu(menuName = "Justin/Dialogue Data", fileName = "New Dialogue Date")]
+ public class DialogueData : ScriptableObject
+    {
+        [Header("對話者名稱")]
+        public string dialogueName;
+        [Header("對話者內容"), TextArea(2, 10)]
+        public string[] dialogueContents;
+    }
+}
+
+
---
+++ 202211253D_Kid/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using TMPro;
+namespace Just
+{
+    /// <summary>
+    /// 對話系統
+    /// </summary>
+    public class DialogueSystem : MonoBehaviour
+    {
+        [SerializeField, Header("對話間隔"), Range(0, 0.5f)]
+        private float dialogueIntervalTime = 0.1f;
+
+        private WaitForEndOfFrame dialogueInterval;
+        private CanvasGroup groupDialogue;
+        private TextMeshProUGUI textName;
+        private TextMeshProUGUI textContont;
+        private GameObject goTriangle;
+    }
+}
+
+
