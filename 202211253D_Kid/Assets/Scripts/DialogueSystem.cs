a531ba167142b45ca095d7f74fb11b70d9160937
justin28668221 <96966547+fat28668221@users.noreply.github.com>
2022-12-02 16:12:40 +0800

    20221202Finish

--- 202211253D_Kid/Assets/Scripts/DialogueSystem.cs
+++ 202211253D_Kid/Assets/Scripts/DialogueSystem.cs
@@ -7,14 +7,31 @@ namespace Justin
     /// </summary>
     public class DialogueSystem : MonoBehaviour
     {
+
+        #region 資料區
         [SerializeField, Header("對話間隔"), Range(0, 0.5f)]
         private float dialogueIntervalTime = 0.1f;
+        [SerializeField, Header("開頭對話")]
+        private DialogueData dialogueOpening;
 
-        private WaitForEndOfFrame dialogueInterval;
+        private WaitForSeconds dialogueInterval => new WaitForSeconds(dialogueIntervalTime);
+
         private CanvasGroup groupDialogue;
         private TextMeshProUGUI textName;
         private TextMeshProUGUI textContont;
         private GameObject goTriangle;
+        #endregion
+
+        #region 事件
+        private void Awake()
+        {
+            groupDialogue = GameObject.Find("畫布對話系統").GetComponent<CanvasGroup>();
+            textName = GameObject.Find("對話者名稱").GetComponent<TextMeshProUGUI>();
+            textContont = GameObject.Find("對話內容").GetComponent<TextMeshProUGUI>();
+            goTriangle = GameObject.Find("對話完成圖示");
+            goTriangle.SetActive(false);
+        } 
+        #endregion
     }
 }
 
